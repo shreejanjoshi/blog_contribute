b572885eb2ba34a162c81378e6adff0c60ccc467
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-02-08 12:48:38 +0100

    updated code

--- blog/src/components/home/homeLayout.tsx
+++ blog/src/components/home/homeLayout.tsx
@@ -9,7 +9,7 @@ interface HomeLayoutProps {
   blogs?: Blog[];
 }
 
-const HomeLayout = ({ title, link, blogs, menus }: HomeLayoutProps) => {
+const HomeLayout = ({ title, link, blogs }: HomeLayoutProps) => {
   return (
     <>
       <section className="w-full  mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col  items-center justify-center">
