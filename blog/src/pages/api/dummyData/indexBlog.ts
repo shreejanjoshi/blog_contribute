15128e7bba9d3431b49a82b7359d8007e496a55a
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-05-04 20:46:19 +0200

    add blogs

--- blog/src/pages/api/dummyData/indexBlog.ts
+++ blog/src/pages/api/dummyData/indexBlog.ts
@@ -19,7 +19,7 @@ const blogs = [
     title: "Mijn Eerste Week bij Contribute – Een Nieuwe Start",
     short:
       "Mijn stage bij Contribute is officieel begonnen! Het was een boeiende eerste week vol nieuwe ervaringen, kennismakingen en intensieve trainingen. Hier is een overzicht van mijn eerste dagen bij het bedrijf.",
-    categories: ["Internship", "Learning", "Training"],
+    categories: ["Internship", "Learning", "Training", "Tia4Me"],
     fullText:
       "Mijn eerste week bij Contribute was een spannende en leerzame ervaring. Op de eerste dag maakten we kennis met alle collega’s en kregen we onze laptops waarop we de nodige software installeerden. We kregen ook een eerste introductie in Oracle APEX, een platform dat binnen Contribute veel gebruikt wordt.De tweede, derde en vierde dag stonden in het teken van SQL en PL/SQL training. We leerden hoe databases werken, hoe we gegevens kunnen opvragen en bewerken met SQL en hoe we met PL/SQL geavanceerde databasefunctionaliteiten kunnen implementeren.",
     fullText2:
@@ -37,7 +37,7 @@ const blogs = [
     title: "Mijn Tweede Week bij Contribute – Dieper in Oracle APEX",
     short:
       "De tweede week van mijn stage bij Contribute was gevuld met zelfstudie en verdieping in Oracle APEX. Ik leerde zelfstandig werken en kreeg een beter inzicht in het bouwen van een blogwebsite binnen APEX.",
-    categories: ["Internship", "Oracle APEX", "Self-Study"],
+    categories: ["Internship", "Oracle APEX", "Self-Study", "Tia4Me"],
     fullText:
       "Na een boeiende eerste week bij Contribute, waarin we de basis van Oracle APEX en SQL verkenden, doken we deze week dieper in de wereld van APEX. De focus lag vooral op zelfstudie en zelfstandig leren. Dit betekende dat we zelf op onderzoek uitgingen, documentatie doornamen en hands-on ervaring opdeden door met de tool te werken. Eén van de grootste uitdagingen van deze week was het bouwen van een blogwebsite in Oracle APEX. Dit gaf me niet alleen inzicht in hoe je een functionele webapplicatie opzet, maar ook in hoe APEX werkt als low-code ontwikkelplatform.",
     fullText2:
@@ -53,7 +53,7 @@ const blogs = [
     id: 3,
     title: "Start van het 4me-Tia Project",
     short: "Eerste stappen in het 4me-Tia project",
-    categories: ["Internship", "Project Work", "Database"],
+    categories: ["Internship", "Project Work", "Database", "Tia4Me"],
     fullText:
       "Deze week zijn we begonnen aan ons eerste project genaamd 4me-Tia. We hebben tickets aangemaakt in JIRA en verbinding gemaakt met de database. Het project draait om het ontwikkelen van APEX-schermen om administratieve taken te vereenvoudigen. We hebben gewerkt aan verschillende onderdelen van de applicatie. Eerst hebben we de loginpagina opgezet om ervoor te zorgen dat beheerders veilig kunnen inloggen. Daarna hebben we de pagina gemaakt waar een organisatie gekozen kan worden, zodat alleen relevante gegevens worden weergegeven. Vervolgens hebben we de medewerkerspagina ontwikkeld om een overzicht te tonen van alle werknemers binnen een organisatie en een klantenpagina om alle klanten weer te geven.",
     fullText2:
@@ -69,7 +69,7 @@ const blogs = [
     id: 4,
     title: "Verder werken aan de Holiday Calendar",
     short: "Holiday Calendar bewerkbaar maken en werken met SQL Views",
-    categories: ["Internship", "Oracle APEX", "SQL"],
+    categories: ["Internship", "Oracle APEX", "SQL", "Tia4Me"],
     fullText:
       "Deze week heb ik verder gewerkt aan de Holiday Calendar en ervoor gezorgd dat deze nu bewerkbaar en verwijderbaar is. Mijn belangrijkste taak was het selecteren van een specifieke klant binnen een organisatie en vervolgens de bijbehorende vakantiekalendergegevens weergeven. Als er geen klantgebonden vakantiedata beschikbaar waren, moest in plaats daarvan de vakantiedata van de organisatie zelf worden getoond. Daarnaast moesten we views maken voor alle gegevens en deze gebruiken om data weer te geven in APEX",
     fullText2:
@@ -87,7 +87,7 @@ const blogs = [
     id: 5,
     title: "Verbeteringen aan de Lay-out en Nieuwe Functionaliteiten",
     short: "UI verbeteringen en API-integratie",
-    categories: ["Internship", "UI/UX Design", "API Development"],
+    categories: ["Internship", "UI/UX Design", "API Development", "Tia4Me"],
     fullText:
       "Deze week hebben we voornamelijk gewerkt aan het verbeteren van de lay-out en look & feel van de applicatie. We hebben een logo toegevoegd, de kleuren aangepast en ervoor gezorgd dat de interface er visueel aantrekkelijker en professioneler uitziet. Voor de ontwerpconcepten hebben we inspiratie gehaald van de website van de Three-Headed Giant, wat hielp bij het bepalen van de stijl en structuur van de applicatie.",
     fullText2:
@@ -107,7 +107,13 @@ const blogs = [
     id: 6,
     title: "Beveiliging van de Applicatie en Rolbeheer",
     short: "Rollen, rechten en gebruikersbeheer instellen",
-    categories: ["Internship", "Security", "User Management", "Data Modeling"],
+    categories: [
+      "Internship",
+      "Security",
+      "User Management",
+      "Data Modeling",
+      "Tia4Me",
+    ],
     fullText:
       "Deze week stond volledig in het teken van de beveiliging van de applicatie. Mijn taak was om ervoor te zorgen dat alleen gebruikers met de juiste rechten toegang krijgen tot specifieke pagina’s, zoals de beveiligingspagina waar gebruikers kunnen worden beheerd. Hiervoor heb ik eerst een datamodel ontworpen, dat duidelijk maakt welke gebruiker tot welke organisatie behoort en welke rol hij of zij daarin heeft. Daarna heb ik de bijbehorende tabellen aangemaakt en gevuld met testdata. Op basis daarvan kon ik de Security Page bouwen. Op deze pagina zie je een overzicht van alle gebruikers, hun rollen en de organisaties waartoe ze toegang hebben.",
     fullText2:
@@ -127,7 +133,14 @@ const blogs = [
     id: 7,
     title: "Security Page Uitbreiden en Super Admin Logica",
     short: "Beveiliging uitbreiden en automatische organisatiekoppeling",
-    categories: ["Internship", "Security", "Triggers", "Bug Fixing", "Demo"],
+    categories: [
+      "Internship",
+      "Security",
+      "Triggers",
+      "Bug Fixing",
+      "Demo",
+      "Tia4Me",
+    ],
     fullText:
       "Deze week heb ik verder gewerkt aan de security pagina, met de focus op het verbeteren van het design en de gebruikerservaring. Een belangrijk onderdeel was het automatiseren van de organisatiekoppeling voor gebruikers met de rol 'Super Administrator'. Hiervoor heb ik een trigger geschreven die, zodra een gebruiker deze rol krijgt, automatisch alle beschikbare organisaties koppelt aan deze gebruiker. Hierdoor krijgt een super admin altijd toegang tot alles, zonder handmatig extra instellingen te doen.",
     fullText2:
@@ -150,9 +163,10 @@ const blogs = [
     categories: [
       "Internship",
       "New Project",
-      "File Upload",
+      "File",
       "Feature Development",
-      "Client Feedback",
+      "Feedback",
+      "Centerfield",
     ],
     fullText:
       "Deze week zijn we gestart met een nieuw project waarin we bestaande functionaliteiten moesten uitbreiden. Het project is een gesprekkenmodule waar HR/coördinatoren gesprekken met werknemers kunnen inplannen. Mijn taak was om ervoor te zorgen dat bij het aanmaken van een gesprek documenten zoals Word, PDF of Excel kunnen worden toegevoegd. Deze bijlagen moeten bij het gesprek bewaard worden en consulteerbaar zijn, en bij klikken openen in een aparte tab. Daarnaast heb ik ervoor gezorgd dat gebruikers de toegevoegde bestanden ook kunnen downloaden.",
@@ -173,7 +187,13 @@ const blogs = [
     id: 9,
     title: "Country Template Functionaliteit Voltooid",
     short: "Landgebonden feestdagen als sjabloon voor organisaties",
-    categories: ["Internship", "Feature Development", "Calendar", "Templates"],
+    categories: [
+      "Internship",
+      "Feature Development",
+      "Calendar",
+      "Templates",
+      "Tia4Me",
+    ],
     fullText:
       "Deze week hebben we gewerkt aan het afronden van het project door alle 'nice to have'-functionaliteiten te implementeren. De belangrijkste toevoeging was de nieuwe functionaliteit voor het beheren van country templates. Elk land heeft namelijk zijn eigen feestdagen, en sommige zijn uniek per land. Om dit proces te vergemakkelijken, kunnen super admins nu landen aanmaken in het country template gedeelte, en vervolgens feestdagen toevoegen in de country holidays pagina. Zo ontstaat er per land een sjabloon van feestdagen.",
     fullText2:
@@ -185,6 +205,93 @@ const blogs = [
     image3:
       "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1743967519/WhatsApp_Image_2025-04-06_at_19.50.00_ad8dfd4a_nelv9t.jpg",
     createdAt: "2025-04-06",
+    authorName: "Shreejan Joshi",
+    authorImage:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1738960869/IMG_3150_web_gmcitd.png",
+  },
+  {
+    id: 10,
+    title: "Gespreksmodule Uitbreiden met Meerdere Bestanden",
+    short: "Upload & download meerdere documenten per gesprek",
+    categories: ["Internship", "Feature", "Feedback", "File", "Centerfield"],
+    fullText:
+      "Deze week ben ik opnieuw aan de slag gegaan met het Centerfield-project, meer specifiek aan de gespreksmodule. Op basis van feedback die we kregen, moest ik de bestaande functionaliteit verbeteren. Het is nu mogelijk om meerdere bestanden toe te voegen aan een gesprek, ook nadat het gesprek al eerder opgeslagen werd. Hierdoor wordt het gebruik flexibeler en gebruiksvriendelijker voor HR en coördinatoren.",
+    fullText2:
+      "De toegevoegde bestanden zijn ook zichtbaar op de documentpagina, zodat alles overzichtelijk blijft. Belangrijk hierbij is dat documenten enkel verwijderd kunnen worden op de pagina waar ze oorspronkelijk werden geüpload. Dit zorgt ervoor dat we volledige controle houden over waar wijzigingen aan bestanden worden gedaan. Met deze verbeteringen is de gespreksmodule een stuk sterker en beter afgestemd op de noden van de gebruikers.",
+    image:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746380867/premium_photo-1677402408071-232d1c3c3787_a5at1m.avif",
+    image2:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746365616/WhatsApp_Image_2025-05-04_at_15.23.32_fb710fd2_teb7wv.jpg",
+    image3:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746365611/WhatsApp_Image_2025-05-04_at_15.23.32_6d51bd7b_dqjgif.jpg",
+    createdAt: "2025-04-13",
+    authorName: "Shreejan Joshi",
+    authorImage:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1738960869/IMG_3150_web_gmcitd.png",
+  },
+  {
+    id: 11,
+    title: "Back to Tia4Me – Nieuwe Features & Projectwissels",
+    short: "Nieuwe schermen, verbeteringen en goed plannen",
+    categories: ["Internship", "Tia4Me", "Planning", "Tia4Me"],
+    fullText:
+      "Deze week werkten we opnieuw aan het Tia4Me-project. We voegden enkele nieuwe features toe die waren aangevraagd. Dit omvatte het toevoegen van een nieuw scherm en het verbeteren van een aantal bestaande schermen om de functionaliteit te versterken.",
+    fullText2:
+      "We moesten vaak schakelen tussen verschillende projecten, omdat we ook aan Centerfield moesten werken. Dat was best een uitdaging, omdat we weinig tijd hadden om aan de nieuwe features te wennen. We hebben dit opgelost door een goede planning te maken en onze tijd efficiënt te verdelen over beide projecten.",
+    image:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746382402/photo-1501504905252-473c47e087f8_ccax61.avif",
+    image2:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746382213/WhatsApp_Image_2025-05-04_at_20.01.25_05d4c7e1_kz83cw.jpg",
+    image3:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746382213/WhatsApp_Image_2025-05-04_at_20.01.25_3e88d7e6_munyg5.jpg",
+    createdAt: "2025-04-20",
+    authorName: "Shreejan Joshi",
+    authorImage:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1738960869/IMG_3150_web_gmcitd.png",
+  },
+  {
+    id: 12,
+    title:
+      "Twee Tickets Succesvol Afgerond – Synchronisatie & Documentweergave",
+    short:
+      "Deze week heb ik twee belangrijke tickets afgewerkt voor het Centerfield-project.",
+    categories: ["Internship", "Centerfield", "File"],
+    fullText:
+      "Deze week heb ik twee taken succesvol afgerond binnen het Centerfield-project. Het eerste ticket ging over het synchroniseren van werknemers uit de user_management module naar Centerfield. Ik zorgde ervoor dat de synchronisatie correct verliep, zodat alle gebruikers automatisch worden overgezet en zichtbaar zijn in het systeem.",
+    fullText2:
+      "Het tweede ticket betrof het toevoegen van een overzicht van documenten aan de gespreksdocumentpagina in de Frontoffice. Tot nu toe waren geüploade bestanden enkel zichtbaar in de Backoffice, maar vanaf nu kunnen gebruikers in de Frontoffice ook documenten bekijken en downloaden per gesprekstype (zoals jaarlijks, tussentijds of exit). Deze functionaliteit hergebruikt grotendeels de logica van de Backoffice, wat het makkelijker maakt om consistentie te behouden.",
+    image:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746383351/premium_photo-1661876775049-d56ae3c0a677_e8egty.avif",
+    image2:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746383261/WhatsApp_Image_2025-05-04_at_20.21.15_97d96770_slbqyx.jpg",
+    image3:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746383261/WhatsApp_Image_2025-05-04_at_20.21.14_42f9569b_redchj.jpg",
+    createdAt: "2025-04-27",
+    authorName: "Shreejan Joshi",
+    authorImage:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1738960869/IMG_3150_web_gmcitd.png",
+  },
+  {
+    id: 13,
+    title: "Frontoffice Updates – Documentenweergave & Asset Ontvangstdatum",
+    short:
+      "Twee nuttige verbeteringen voor de gebruikerservaring in de Frontoffice.",
+    categories: [
+      "Internship",
+      "Frontoffice",
+      "UI/UX Design",
+      "Database",
+      "Centerfield",
+    ],
+    fullText:
+      "Deze week heb ik gewerkt aan twee verbeteringen binnen de Frontoffice. Het eerste ticket ging over het zichtbaar maken van documenten die aan gesprekken zijn gekoppeld. Gebruikers kunnen nu in de Frontoffice, net zoals in de Backoffice, geüploade bestanden bekijken en downloaden per gespreksdocument (zoals Exit, Jaarlijks, Tussentijds, enz.). Ik heb dezelfde logica hergebruikt als in de Backoffice en het overzicht onderaan de vragenlijst geplaatst met bestandsnaam en downloadknop.",
+    fullText2:
+      "Daarnaast heb ik ook gewerkt aan een verbetering in de assetpagina. Voorheen was het enkel mogelijk om een asset als ontvangen te markeren, maar er werd geen datum opgeslagen. Nu kunnen gebruikers bij het aanduiden van ontvangst ook een ondertekendatum selecteren via een datumveld. Deze verbeteringen zorgen ervoor dat data vollediger en correcter wordt geregistreerd in het systeem.",
+    image:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746384276/photo-1611302457661-d24c21494f2a_1_ilenxr.avif",
+    image2:
+      "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1746384047/WhatsApp_Image_2025-05-04_at_20.36.53_3f6370e4_jfj6cw.jpg",
+    createdAt: "2025-05-04",
     authorName: "Shreejan Joshi",
     authorImage:
       "https://res.cloudinary.com/dl3qqfbrh/image/upload/v1738960869/IMG_3150_web_gmcitd.png",
